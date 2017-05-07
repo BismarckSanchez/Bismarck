1b7c3fcb616431447d9b5c19319c4e1c2e903cea
BismarckSanchez <bismarcksanchez1234@gmail.com>
2017-05-06 22:40:29 -0500

    Add files via upload

---
+++ Empleado.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package taller;
+
+import taller.Persona;
+import java.util.Date;
+
+/**
+ *
+ * @author bismarck
+ */
+public class Empleado  extends Persona{
+    double salario;
+    Date fecha_entrada;
+}
